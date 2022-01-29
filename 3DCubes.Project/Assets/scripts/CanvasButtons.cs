1e8495f88c362428c29d5d57e85f343699ad8b96
VladIsLove16 <zalezshi@gmail.com>
2022-01-30 00:06:17 +0500

    23

--- 3DCubes.Project/Assets/scripts/CanvasButtons.cs
+++ 3DCubes.Project/Assets/scripts/CanvasButtons.cs
@@ -17,15 +17,25 @@ private void Start(){
     {
         if(PlayerPrefs.GetString("music")=="Yes")
         GetComponent<AudioSource>().Play();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("Main");
     }
-    private void OnGUI() {
-        
-    
-	if (GUI.Button(new Rect(20, 100, 100, 20), "Перезапустить")) {
-		SceneManager.LoadScene("SampleScene");
-	}
-}
+     public void LoadShop()
+    {
+        if(PlayerPrefs.GetString("music")=="Yes")
+        GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene("Shop");
+    }
+    public void CloseShop()
+    {
+        if(PlayerPrefs.GetString("music")=="Yes")
+        GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene("Main");
+    }  
+      //private void OnGUI() {
+	//if ( && GUI.Button(new Rect(20, 100, 100, 20), "Перезапустить")) {
+		//SceneManager.LoadScene("Main");
+	//}}
+
     public void Insta( )
     {
         if(PlayerPrefs.GetString("music")=="Yes")
--- 3DCubes.Project/Assets/scripts/GameController.cs
+++ 3DCubes.Project/Assets/scripts/GameController.cs
@@ -11,11 +11,16 @@ public class GameController : MonoBehaviour
     private float camMoveToYPosition,camMoveSpeed=2f;
     public Text scoreTxt;
     public Transform cubeToPlace;
-    public GameObject cubeToCreate, allCubes,vfx;
+    public Material[] Materials;
+    public GameObject  allCubes,vfx;
+    public GameObject[] cubesToCreate; 
+    public List<Material> posibleMaterials=new List<Material>();
+    public List<GameObject> posibleCubesToCreate=new List<GameObject>();
 
     public GameObject[] canvasStartPage;
     private Rigidbody allCubesRb;
     private bool firstCube;
+    private int ID;
     private bool isLose=false;
     public Color[] bgColors;
     private Color toCameraColor;
@@ -38,7 +43,24 @@ public class GameController : MonoBehaviour
     private void Start()
                 
     {
-        scoreTxt.text="<size=15><color=#C31414>Best Score:</color></size>"+ PlayerPrefs.GetInt("score")+"<size=15><color=#1BB70F>Now:</color></size>";
+        if(PlayerPrefs.GetInt("score")<2)
+        {}
+        else if (PlayerPrefs.GetInt("score")<4)
+        AddPossibleCubes(1);
+         else if (PlayerPrefs.GetInt("score")<7)
+        AddPossibleCubes(2);
+         else if (PlayerPrefs.GetInt("score")<11)
+        AddPossibleCubes(3);
+         else if (PlayerPrefs.GetInt("score")<13)
+        AddPossibleCubes(4);
+         else if (PlayerPrefs.GetInt("score")<16)
+        AddPossibleCubes(5);
+         else if (PlayerPrefs.GetInt("score")<19)
+        AddPossibleCubes(6);
+         else if (PlayerPrefs.GetInt("score")<21)
+        AddPossibleCubes(7);
+
+        scoreTxt.text="<size=15><color=#C31414>Best score:</color></size>"+ PlayerPrefs.GetInt("score")+"<size=15><color=#1BB70F>Now:</color></size>";
       
         toCameraColor = Camera.main.backgroundColor;
      mainCam = Camera.main.transform;
@@ -65,9 +87,10 @@ public class GameController : MonoBehaviour
                 }
                 
            GameObject newCube = Instantiate(
-                cubeToCreate,
+                cubesToCreate[ID],
                 cubeToPlace.position,
                 Quaternion.identity) as GameObject;
+                
             newCube.transform.SetParent(allCubes.transform);
             nowCube.setVector(cubeToPlace.position);
             AllCubesPositions.Add(nowCube.getVector());
@@ -81,10 +104,11 @@ camMoveToYPosition = 4.9f + nowCube.y;
         GetComponent<AudioSource>().Play();
             SpawnPositions();
            MoveCameraChangeBg();
-          
+           StopCoroutine(showCubePlace);
+           showCubePlace=StartCoroutine(ShowCubePlace());
         }
 
-        if(!isLose && allCubesRb.velocity.magnitude>0.1f)
+        if(!isLose && allCubesRb.velocity.magnitude>0.15f)
         {
             Destroy(cubeToPlace.gameObject);
             isLose = true;
@@ -115,12 +139,11 @@ camMoveToYPosition = 4.9f + nowCube.y;
       if(PlayerPrefs.GetInt("score")<maxY)
       PlayerPrefs.SetInt("score",maxY);
 
-      scoreTxt.text="<size=15><color=#C31414>Best Score:</color></size>"+ PlayerPrefs.GetInt("score")+"<size=15><color=#1BB70F>Now:</color></size>"+Convert.ToString(maxY);
+      scoreTxt.text="<size=15><color=#C31414>Best score:</color></size>"+ PlayerPrefs.GetInt("score")+"<size=15><color=#1BB70F>Now:</color></size>"+Convert.ToString(maxY);
       
-
         maxHor = maxX > maxZ ? maxX : maxZ;
        if(maxHor%3==0 && prevCountMaxHorizontal != maxHor ){
-            mainCam.localPosition -= new Vector3(0, 0, 2.5f);
+            mainCam.localPosition += new Vector3(2f, 0, 2.5f);
             prevCountMaxHorizontal = maxHor;
             }
 
@@ -130,31 +153,14 @@ camMoveToYPosition = 4.9f + nowCube.y;
 toCameraColor = bgColors[1];
        }
             if (maxY >= 3){
-                
                 toCameraColor = bgColors[0];
             }
             
             if (maxY >= 7){
-                 
                 toCameraColor = bgColors[2];
             }
-            
-         
-
         
-         
-            
-
-            
-       
-
-
-
-       
-
-
     }
-
 
     struct CubePos
     {
@@ -184,15 +190,21 @@ toCameraColor = bgColors[1];
         {
             SpawnPositions();
             yield return new WaitForSeconds(CubeChangePlaceSpeed);
+            ID=UnityEngine.Random.Range(0,posibleCubesToCreate.Count);
+            cubeToPlace.GetComponent<MeshRenderer>().material=posibleMaterials[ID];
+
+          //  Debug.Log(ID+" and "+posibleCubesToCreate.Count);
+            
+            
         }
     }
-
-   
-
-
-
-    
-
+    private void AddPossibleCubes(int Num)
+    {
+    for(int i=1;i<=Num;i++){
+    posibleMaterials.Add(Materials[i-1]);
+    posibleCubesToCreate.Add(cubesToCreate[i-1]);
+    }
+    }
 
     private bool isPositionEmpty(Vector3 targetPos)
     {
@@ -205,10 +217,6 @@ toCameraColor = bgColors[1];
         
         return true;
     }
-
-  
-    
-       
     
     private void SpawnPositions()
     {
@@ -237,6 +245,7 @@ toCameraColor = bgColors[1];
             isLose = true;
         else
             cubeToPlace.position = positions[0];
+         //GetComponent<MeshRenderer>().material=posibleCubesToCreate[1].GetComponent<MeshRenderer>.material;
 
 
     }
